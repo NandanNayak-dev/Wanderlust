6e127d2e6c95335f0d84cb60ebf483cb57008ae0
Nandan Nayak <nandannayak2102@gmail.com>
2026-01-12 22:06:31 +0530

    Formatting

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ app.get("/listings/:id/edit",async(req,res)=>{
 })
 app.put("/listings/:id",async(req,res)=>{
   let id=req.params.id;
+  console.log(req.body.listing);
 await Listing.findByIdAndUpdate(id,{...req.body.listing});
   
   res.redirect('/listings/'+id);
