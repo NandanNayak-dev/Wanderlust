762494b4b1e4f72659af69f3f3a8205917a97e05
Nandan Nayak <nandannayak2102@gmail.com>
2026-01-14 06:55:37 +0530

    Added Basic client side validations

--- app.js
+++ app.js
@@ -12,17 +12,16 @@ app.use(methodOverride("_method"));
 const ejsMate = require("ejs-mate");
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname,"public")))
-
-
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
+//------------------MONGOOSE CONNECTION------------------------
 
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
 main().then(() => console.log("connected to database")).catch((err) => console.log(err));
 //-----------------------------------------------------------------------------------
-app.listen(8080, () => {
-  console.log("server is listening to port 8080");
-});
+
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
@@ -42,49 +41,68 @@ app.get("/", (req, res) => {
 
 
 //Index Route
-app.get("/listings", async (req, res) => {
+app.get("/listings",wrapAsync( async (req, res) => {
   let allListings = await Listing.find({});
   res.render("listings/index", { allListings });
-})
+}))
 
 //New Route
 app.get("/listings/new", (req, res) => {
   res.render("listings/new");
 })
 //Show Route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id",wrapAsync( async (req, res) => {
   let listing = await Listing.findById(req.params.id);
   console.log(listing);
   res.render("listings/show", { listing });
-})
+}))
 
 //Create Route
-app.post("/listings", async (req, res) => {
+app.post("/listings",wrapAsync( async (req, res) => {
+  if(!req.body.listing) {
+    throw new ExpressError(400,"Invalid Listing Data");
+  }
    let newListing = new Listing(req.body.listing);
    await newListing.save();
    console.log(newListing);
    res.redirect("/listings");
   
-})
-
-app.get("/listings/:id/edit",async(req,res)=>{
+}))
+//Edit Route
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
   let id=req.params.id;
   const listing= await Listing.findById(id);
   console.log(listing);
   res.render("listings/edit",{listing});
-})
-app.put("/listings/:id",async(req,res)=>{
+}))
+//Update Route
+app.put("/listings/:id",wrapAsync(async(req,res)=>{
   let id=req.params.id;
+  if(!req.body.listing) {
+    throw new ExpressError(400,"Invalid Listing Data");
+  }
   console.log(req.body.listing);
-await Listing.findByIdAndUpdate(id,{...req.body.listing});
-  
+  await Listing.findByIdAndUpdate(id,{...req.body.listing});
   res.redirect('/listings/'+id);
-})
-app.delete("/listings/:id",async(req,res)=>{
+}))
+//Delete Route
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
   let id=req.params.id;
   let deletedListing= await Listing.findByIdAndDelete(id);
   res.redirect("/listings");
+}))
+app.use((req,res,next)=>{
+  next(new ExpressError(404,"Page Not Found"));
+});
+
+app.use((err,req,res,next)=>{
+  let {statusCode=500,message="Something went wrong"}=err;
+  res.status(statusCode).render("error",{message});
+  // res.status(statusCode).send(message);
   
-})
+});
 
 
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
