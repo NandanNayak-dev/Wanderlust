e30fa9e129da936f5548b0217fe0671e0e90b8fd
Nandan Nayak <nandannayak2102@gmail.com>
2026-02-28 18:35:08 +0530

    feat(listings): add create/edit/show flow for listings

--- controllers/listings.js
+++ controllers/listings.js
@@ -22,9 +22,13 @@ module.exports.showListing=async (req, res) => {
 }
 //Create
 module.exports.createListing=async (req, res) => {
-  
+  let url=req.file.path;
+  let filename=req.file.filename;
+  console.log(url,"..",filename)
    let newListing = new Listing(req.body.listing);
    newListing.owner=req.user._id;
+   newListing.image.url=url;
+   newListing.image.filename=filename;
    await newListing.save();
    req.flash("success","Successfully made a new listing");
    res.redirect("/listings");
@@ -43,8 +47,18 @@ module.exports.renderEditForm=async(req,res)=>{
 //Update
 module.exports.updateListing=async(req,res)=>{
   let id=req.params.id;
+  
 
-  await Listing.findByIdAndUpdate(id,{...req.body.listing});
+  let listing=await Listing.findByIdAndUpdate(id,{...req.body.listing});
+  if(typeof req.file!=='undefined' ){
+    let url=req.file.path;
+    let filename=req.file.filename;
+    listing.image.url=url;
+    listing.image.filename=filename;
+
+  await listing.save();
+  }
+
   req.flash("success","Successfully updated the listing");
   res.redirect('/listings/'+id);
 }
--- routes/listing.js
+++ routes/listing.js
@@ -7,15 +7,14 @@ const { isLoggedIn,isOwner,validateListing } = require("../middleware.js");
 
 const listingController=require("../controllers/listings.js");
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const {storage}=require("../cloudConfig.js");
+const upload = multer({storage})
+
 
 router.route("/")
 //Contains index route and post route
 .get(wrapAsync(listingController.index))
-// .post(isLoggedIn,validateListing,wrapAsync(listingController.createListing));
-.post(upload.single('listing[image]'),(req,res)=>{
-    res.send(req.file);
-})
+.post(isLoggedIn,upload.single('listing[image]'),validateListing,wrapAsync(listingController.createListing));
 
 //New Route
 router.get("/new",isLoggedIn,listingController.renderNewForm);
@@ -23,7 +22,7 @@ router.get("/new",isLoggedIn,listingController.renderNewForm);
 router.route("/:id")
 //Contains show route, put route and delete route
 .get(wrapAsync(listingController.showListing))
-.put(isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing))
+.put(isLoggedIn,isOwner,upload.single('listing[image]'),validateListing,wrapAsync(listingController.updateListing))
 .delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListing))
 
 //Edit Route
